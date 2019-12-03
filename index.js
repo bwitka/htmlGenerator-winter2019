8e4230d3c8f8bdd6692e7ba44bdc978446272415
Beau Witka <bwitka@gmail.com>
2019-12-03 12:16:37 -0800

    Connect website to Atlas database via index.js and index.html

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// require index.js file
+const auth = require("./assets/auth.js");
+
 // import express (after npm install express)
 const express = require("express");
 
@@ -10,14 +13,30 @@ const http = require("http").Server(app);
 // Create a variable that will hold the port number we want to use.
 const port = 3000;
 
-// Pass the port variable to the listen function for the HTTP server.
-http.listen(port);
-
 // Include Body Parser, which comes with Express.js.
 const bodyParser = require("body-parser");
+
+// Pass the port variable to the listen function for the HTTP server.
+http.listen(port);
 
 // Message to tell the developer that Express.js is now running.
 console.log(`Express server now running on port ${port}!`);
 
+console.log(auth.getDBURL());
+
+/***** ROUTES ******/
 // Directory to load when someone requests the route name.
 app.use(express.static("client/"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// app.post(path, callback [, callback ...])
+// note: using shorthand for request and response [i.e. req, res]
+app.post("/login", (req, res) => {
+  let requestUsername = req.body;
+  let requestPassword = req.body;
+
+  console.log(requestUsername, requestPassword);
+
+  response.sendStatus(200);
+});
